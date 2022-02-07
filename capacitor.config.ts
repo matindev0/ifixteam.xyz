11c0bc2f353cf61e9ff0508e692c8376c6e34b93
matindev0 <matinnajat1700@gmail.com>
2022-02-07 21:58:45 +0300

    iFix

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'xyz.ifixteam.convertwebsitetoapp',
+  appName: 'iFix',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
